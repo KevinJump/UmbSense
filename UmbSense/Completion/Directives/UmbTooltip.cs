1e3361dda457f3f2e8e9b2e491f1ba1ced8f2822
leekelleher <leekelleher@gmail.com>
2019-08-19 18:13:45 +0100

    Added `umb-tooltip` directive

---
+++ UmbSense/Completion/Directives/UmbTooltip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, "umb-tooltip")]
+    [ContentType("htmlx")]
+    class UmbTooltip : BaseCompletion
+    {
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { "event", "Set the $event from the target element to position the tooltip relative to the mouse cursor." },
+        };
+    }
+}
