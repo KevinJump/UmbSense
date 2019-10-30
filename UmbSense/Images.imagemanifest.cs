e4d031c1395075abecc4052393ca43a3a6755b61
leekelleher <leekelleher@gmail.com>
2019-10-30 21:43:13 +0000

    Tinifed umblogo.png
    This is a micro-optimisation, I ran the umblogo.png through TinyPNG and
    it compressed down from 520 bytes to 330 bytes.
    
    I know it's petty, but maybe every byte counts?

--- UmbSense/Images.imagemanifest.cs
+++ UmbSense/Images.imagemanifest.cs
@@ -1,15 +1,12 @@
-﻿using Microsoft.VisualStudio.Imaging.Interop;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using Microsoft.VisualStudio.Imaging.Interop;
 
 namespace UmbSense
 {
     public static class Images
     {
         private static Guid _guid = new Guid("{3b2a6f15-801e-4a0a-8dc2-4ae02ceb847f}");
+
         public static ImageMoniker UmbLogo =>
             new ImageMoniker { Guid = _guid, Id = 0 };
     }
