49bfbbb2d5d603724c0547cbf79f3f7a056e4ee0
Adam Werner <adam.werner@castellantech.com>
2020-10-17 17:27:47 -0400

    Added 'umb-control-group'

---
+++ UmbSense/Completion/Directives/UmbControlGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmbSense.Completion.Directives
+{
+    class UmbControlGroup
+    {
+        internal const string TagName = "umb-control-group";
+    }
+}
