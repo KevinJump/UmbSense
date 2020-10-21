922525ddc93a8a41150561a76f72e02d149fb400
Adam Werner <adam.werner@castellantech.com>
2020-10-21 11:29:54 -0400

    Added 'umb-keyboard-shortcuts-overview'

---
+++ UmbSense/Completion/Directives/UmbKeyboardShortcutsOverview.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+using Microsoft.VisualStudio.Utilities;
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, TagName)]
+    [ContentType("htmlx")]
+    class UmbKeyboardShortcutsOverview : BaseCompletion
+    {
+        internal const string TagName = "umb-keyboard-shortcuts-overview";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { "model", "Keyboard shortcut model. See description and example at https://our.umbraco.com/apidocs/v8/ui/#/api/umbraco.directives.directive:umbkeyboardShortcutsOverview." }
+        };
+    }
+}
