3a2fe4a0b22d0ba08f7a23bfbf8df83653fa8901
Adam Werner <adam.werner@castellantech.com>
2020-10-23 23:37:32 -0400

    Added 'auto-scale'

---
+++ UmbSense/Completion/Directives/AutoScale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, "div")]
+    [ContentType("htmlx")]
+    class AutoScale : BaseCompletion
+    {
+        internal const string TagName = "auto-scale";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { AutoScale.TagName, "Resize div's automatically to fit to the bottom of the screen, as an optional parameter an y-axis offset can be set So if you only want to scale the div to 70 pixels from the bottom you pass \"70\"" }
+        };
+    }
+}
