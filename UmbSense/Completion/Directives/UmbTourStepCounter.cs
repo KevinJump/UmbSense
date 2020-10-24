c70d866ed89899d2ea0004c1e9c1b36845cf57f6
Adam Werner <adam.werner@castellantech.com>
2020-10-23 23:15:52 -0400

    Added 'umb-tour-step' and tour step children
    - added 'umb-tour-step-content'
    - added 'umb-tour-step-counter'
    - added 'umb-tour-step-footer'
    - added 'umb-tour-step-header'

---
+++ UmbSense/Completion/Directives/UmbTourStepCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, TagName)]
+    [ContentType("htmlx")]
+    class UmbTourStepCounter : BaseCompletion
+    {
+        internal const string TagName = "umb-tour-step-counter";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { "current-step", "The current step the tour is on" },
+            { "total-steps", "The current step the tour is on" }
+        };
+    }
+}
---
+++ UmbSense/Completion/Directives/UmbTourStepHeader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, TagName)]
+    [ContentType("htmlx")]
+    class UmbTourStepHeader : BaseCompletion
+    {
+        internal const string TagName = "umb-tour-step-header";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { "title", "The title that needs to be shown" }
+        };
+    }
+}
