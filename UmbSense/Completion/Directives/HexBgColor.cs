81613c8227341144623dafe822edb17e8cf44da8
Adam Werner <adam.werner@castellantech.com>
2020-10-23 23:42:36 -0400

    Added 'hex-bg-color'

---
+++ UmbSense/Completion/Directives/HexBgColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, "*")]
+    [ContentType("htmlx")]
+    class HexBgColor : BaseCompletion
+    {
+        internal const string TagName = "hex-bg-color";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { TagName, "Used to set a hex background color on an element, this will detect valid hex and when it is valid it will set the color, otherwise a color will not be set." }
+        };
+    }
+}
