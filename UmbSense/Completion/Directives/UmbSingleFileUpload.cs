796e78db61405da6d75d88cff848d8b958e37407
Adam Werner <adam.werner@castellantech.com>
2020-10-24 15:22:34 -0400

    Fix 'umb-single-file-upload' as property attribute on ANY element

--- UmbSense/Completion/Directives/UmbSingleFileUpload.cs
+++ UmbSense/Completion/Directives/UmbSingleFileUpload.cs
@@ -1,7 +1,18 @@
-﻿namespace UmbSense.Completion.Directives
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace UmbSense.Completion.Directives
 {
-    class UmbSingleFileUpload
+    [HtmlCompletionProvider(CompletionTypes.Attributes, "*")]
+    [ContentType("htmlx")]
+    class UmbSingleFileUpload : BaseCompletion
     {
         internal const string TagName = "umb-single-file-upload";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { TagName, "A single file upload field that will reset itself based on the object passed in for the rebuild parameter. This is required because the only way to reset an upload control is to replace it's html." }
+        };
     }
 }
