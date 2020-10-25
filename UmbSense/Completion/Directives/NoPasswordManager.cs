dd94dc759b6c260425145c54e0ea8b91288b7d05
Adam Werner <adam.werner@castellantech.com>
2020-10-24 21:59:43 -0400

    Added 'no-password-manager'

---
+++ UmbSense/Completion/Directives/NoPasswordManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, "*")]
+    [ContentType("htmlx")]
+    class NoPasswordManager : BaseCompletion
+    {
+        internal const string TagName = "no-password-manager";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { TagName, "Added attributes to block password manager elements should as LastPass" }
+        };
+    }
+}
