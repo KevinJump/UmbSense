009e06c386da794e7fb0c5eb26fb54fe5037a146
Adam Werner <adam.werner@castellantech.com>
2020-10-24 22:19:16 -0400

    Added 'val-regex'

---
+++ UmbSense/Completion/Directives/ValRegex.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, "*")]
+    [ContentType("htmlx")]
+    class ValRegex : BaseCompletion
+    {
+        internal const string TagName = "val-regex";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { TagName, "A custom directive to allow for matching a value against a regex string. NOTE: there's already an ng-pattern but this requires that a regex expression is set, not a regex string" }
+        };
+    }
+}
