c213639e25197934d06c11766ac3675f3ce01c2b
Adam Werner <adam.werner@castellantech.com>
2020-10-24 22:04:43 -0400

    Added 'resize-to-content'

---
+++ UmbSense/Completion/Directives/ResizeToContent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, "*")]
+    [ContentType("htmlx")]
+    class ResizeToContent : BaseCompletion
+    {
+        internal const string TagName = "resize-to-content";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { TagName, "Resize iframe's automatically to fit to the content they contain" }
+        };
+    }
+}
