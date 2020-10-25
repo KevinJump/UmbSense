155c5e4225113bb270cbb54aa977f0953656de8e
Adam Werner <adam.werner@castellantech.com>
2020-10-24 22:02:58 -0400

    Added 'prevent-enter-submit'

---
+++ UmbSense/Completion/Directives/PreventEnterSubmit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, "*")]
+    [ContentType("htmlx")]
+    class PreventEnterSubmit : BaseCompletion
+    {
+        internal const string TagName = "prevent-enter-submit";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { TagName, "prevents a form from submitting when the enter key is pressed on an input field" }
+        };
+    }
+}
