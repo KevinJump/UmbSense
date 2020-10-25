f268a94e86b3221da0787d6435a36dc3c84cb683
Adam Werner <adam.werner@castellantech.com>
2020-10-24 21:57:56 -0400

    Added 'no-dirty-check'

---
+++ UmbSense/Completion/Directives/NoDirtyCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.WebTools.Languages.Html.Editor.Completion.Def;
+
+namespace UmbSense.Completion.Directives
+{
+    [HtmlCompletionProvider(CompletionTypes.Attributes, "*")]
+    [ContentType("htmlx")]
+    class NoDirtyCheck : BaseCompletion
+    {
+        internal const string TagName = "no-dirty-check";
+
+        protected override Dictionary<string, string> values => new Dictionary<string, string>()
+        {
+            { TagName, "Can be attached to form inputs to prevent them from setting the form as dirty (https://stackoverflow.com/questions/17089090/prevent-input-from-setting-form-dirty-angularjs)" }
+        };
+    }
+}
